0d2651c5d3c4bd1112580e77a508ae38733e61cb
Zach_Gilbert <zach.gilbert2020@gmail.com>
2023-12-02 14:18:00 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
+    // Your JSON string
+    const jsonString = '{"pieces":[{"letter":"A","value":1,"amount":9},{"letter":"B","value":3,"amount":2},{"letter":"C","value":3,"amount":2},{"letter":"D","value":2,"amount":4},{"letter":"E","value":1,"amount":12},{"letter":"F","value":4,"amount":2},{"letter":"G","value":2,"amount":3},{"letter":"H","value":4,"amount":2},{"letter":"I","value":1,"amount":9},{"letter":"J","value":8,"amount":1},{"letter":"K","value":5,"amount":1},{"letter":"L","value":1,"amount":4},{"letter":"M","value":3,"amount":2},{"letter":"N","value":1,"amount":5},{"letter":"O","value":1,"amount":8},{"letter":"P","value":3,"amount":2},{"letter":"Q","value":10,"amount":1},{"letter":"R","value":1,"amount":6},{"letter":"S","value":1,"amount":4},{"letter":"T","value":1,"amount":6},{"letter":"U","value":1,"amount":4},{"letter":"V","value":4,"amount":2},{"letter":"W","value":4,"amount":2},{"letter":"X","value":8,"amount":1},{"letter":"Y","value":4,"amount":2},{"letter":"Z","value":10,"amount":1}]}';
+
+    // Parse the JSON string into a JavaScript object
+    const jsonObject = JSON.parse(jsonString);
+    
+    // Access the "pieces" array
+    const piecesArray = jsonObject.pieces;
+    let currentScore = 0;
 $(document).ready(function(){
 
 
     //generate 7 letters in an array
-    // Your JSON string
-const jsonString = '{"pieces":[{"letter":"A","value":1,"amount":9},{"letter":"B","value":3,"amount":2},{"letter":"C","value":3,"amount":2},{"letter":"D","value":2,"amount":4},{"letter":"E","value":1,"amount":12},{"letter":"F","value":4,"amount":2},{"letter":"G","value":2,"amount":3},{"letter":"H","value":4,"amount":2},{"letter":"I","value":1,"amount":9},{"letter":"J","value":8,"amount":1},{"letter":"K","value":5,"amount":1},{"letter":"L","value":1,"amount":4},{"letter":"M","value":3,"amount":2},{"letter":"N","value":1,"amount":5},{"letter":"O","value":1,"amount":8},{"letter":"P","value":3,"amount":2},{"letter":"Q","value":10,"amount":1},{"letter":"R","value":1,"amount":6},{"letter":"S","value":1,"amount":4},{"letter":"T","value":1,"amount":6},{"letter":"U","value":1,"amount":4},{"letter":"V","value":4,"amount":2},{"letter":"W","value":4,"amount":2},{"letter":"X","value":8,"amount":1},{"letter":"Y","value":4,"amount":2},{"letter":"Z","value":10,"amount":1}]}';
 
-// Parse the JSON string into a JavaScript object
-const jsonObject = JSON.parse(jsonString);
-
-// Access the "pieces" array
-const piecesArray = jsonObject.pieces;
 
 // Loop through the array and access each object's properties
 // piecesArray.forEach(piece => {
@@ -29,9 +31,10 @@ const piecesArray = jsonObject.pieces;
         }
         onDone();
       })
+    //Points  
 function onDone(){
     $(".draggable_box ").draggable({
-        snap:"#snapPoint ",
+        snap:"#snapPoint , #snapHand ",
         snapMode: "inner",
         revert: "invalid",
         snapTolerance: 40,
@@ -42,7 +45,22 @@ function onDone(){
         drop: function(event, ui) {
             var letter = ui.draggable.val("id", ui.draggable).attr('alt');
             console.log(letter);
+            UpdateScore(letter);
         }
     });
 }
 });
+function UpdateScore(letter) {
+    const value = getLetterValue(letter);
+    //console.log(value);
+    currentScore += value;
+    $("#score").html(currentScore);
+}
+function getLetterValue(letter) {
+    const pieceObject = piecesArray.find(piece => piece.letter === letter);
+    if(pieceObject) {
+        return pieceObject.value;
+    } else {
+        return null;
+    }
+}
